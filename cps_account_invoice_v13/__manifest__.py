268137c3de4b4f0bff4f975e5c89b12d9a9fc485
Othmane ABDELOUAFI <a.othmane@cpsmaroc.com>
2020-06-19 10:41:02 +0100

    - Calcul sechage et centrifuge si eau lavage/teinture >0

--- cps_account_invoice_v13/__manifest__.py
+++ cps_account_invoice_v13/__manifest__.py
@@ -11,7 +11,7 @@
     """,
 
     'author': "My Company",
-    'website': "http://www.yourcompany.com",
+    'website': "http://www.netline.ma",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/10.0/odoo/addons/base/module/module_data.xml
