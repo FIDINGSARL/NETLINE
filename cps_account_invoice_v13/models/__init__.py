adc2a4fb39d0f2f5033ef790be2e76459866b504
Othmane ABDELOUAFI <a.othmane@cpsmaroc.com>
2021-02-05 11:27:31 +0100

    - Trello

--- cps_account_invoice_v13/models/__init__.py
+++ cps_account_invoice_v13/models/__init__.py
@@ -4,4 +4,3 @@ from . import cps_account_invoice_sale
 from . import cps_account_invoice_sale_line
 from . import account_move_line
 from . import netline_facturation_journal_bl_report
-from . import amount_to_fr
