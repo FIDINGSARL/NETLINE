6a348b1cebeab0f0e5a7fb302fabf16fc8d80db4
othman <othmane41@gmail.com>
2021-07-15 00:45:36 +0100

    Possibilité d'afficher les bons de commande des DA

--- cps_sale_management/models/cps_purchase_order.py
+++ cps_sale_management/models/cps_purchase_order.py
@@ -9,7 +9,7 @@ class PurchaseOrder(models.Model):
     is_commande = fields.Boolean('Est une commande')
     is_netline = fields.Boolean('Est une commande Netline')
     # reception_id = fields.One2many('netline.reception', 'purchase_order_id', string="receptions")
-    purchase_order_id = fields.Many2one('netline.purchase.da', 'Demande achat')
+    purchase_order_id = fields.Many2one('netline.purchase.da.line', 'Demande achat')
 
 class PurchaseOrderLine(models.Model):
     _inherit = 'purchase.order.line'
